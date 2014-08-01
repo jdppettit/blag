3428f4cb7ad29a70a9546e9c796e12de9df03a28
silverp1 <jpettit@pettitservers.com>
2014-07-31 22:58:33 -0400

    Added bootstrap, made landing and blog pages, css still messed up on blog

--- blag.py
+++ blag.py
@@ -7,7 +7,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Hi, i'm a blag"
+	return render_template("index.html") 
+
+@app.route('/blog')
+def blog():
+	return render_template("blog.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/resume')
+def resume():
+	return render_template("resume.html")
 
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0')
+	app.run(debug=True,host='0.0.0.0',port=5222)
